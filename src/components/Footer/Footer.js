135b21bab22bfe2436072673ffc7c6185e860f4b
Harsh <Harsh7093@gmail.com>
2022-01-25 13:35:16 +0530

    Initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,38 @@
+// import React from 'react'
+
+// const Footer = () => {
+//     return (
+//         <div style={{textAlign:'center',marginBottom: 10}}>
+//             Made By Harsh{" "}
+//         </div>
+//     )
+// }
+
+// export default Footer
+
+
+
+
+
+
+
+const Footer = () => {
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          marginBottom: 10,
+        }}
+      >
+        Made with ♥ by{" "}
+        <a
+          // href="https://www.youtube.com/roadsidecoder"
+          style={{ cursor: "pointer" }}
+        >
+          HARSH
+        </a>
+      </div>
+    );
+  };
+  
+  export default Footer;
